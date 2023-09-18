4f1cc7de599296da5cf9ae41cab18a0f58c4d8e3
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-09-18 21:25:47 +0300

    Change id

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: 'Нічого не знайдено' })
 })
 
 app.use((err, req, res, next) => {
