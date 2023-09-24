8389fbe6886798c773a5f7f72e1a4995e5f8165e
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-09-24 21:00:30 +0300

    rebuild all contacts

--- models/contacts.js
+++ models/contacts.js
@@ -1,91 +1,59 @@
-const fs = require('fs/promises');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs/promises");
 
-const contactsPath = path.resolve(__dirname, 'contacts.json');
+const path = require("path");
+
+// const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "contacts.json"); 
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath, 'utf-8');
+const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 };
 
-const getContactById = async contactId => {
-  if (!contactId) {
-    return undefined;
-  }
-
-  const data = await fs.readFile(contactsPath, 'utf-8');
-  const parsedData = JSON.parse(data);
-
-  const foundContact = parsedData.find(contact => contact.id === contactId);
-
-  return foundContact;
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
 };
 
-const removeContact = async contactId => {
-  if (!contactId) {
-    return undefined;
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
   }
-
-  const data = await fs.readFile(contactsPath, 'utf-8');
-  const parsedData = JSON.parse(data);
-
-  const removeIndex = parsedData.findIndex(contact => contact.id === contactId);
-
-  if (removeIndex === -1) {
-    return undefined;
-  }
-
-  const removedContact = parsedData[removeIndex];
-
-  parsedData.splice(removeIndex, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(parsedData, undefined, 2));
-
-  return removedContact;
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 };
 
-const addContact = async body => {
-  if (Object.keys(body).length <= 2) {
-    return undefined;
-  }
-
-  const data = await fs.readFile(contactsPath, 'utf-8');
-  const parsedData = JSON.parse(data);
-
-  body = { id: uuidv4(), ...body };
-
-  parsedData.push(body);
-
-  fs.writeFile(contactsPath, JSON.stringify(parsedData, undefined, 2));
-
-  return body;
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 };
 
 const updateContact = async (contactId, body) => {
-  if (!contactId) {
-    return undefined;
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
   }
 
-  if (Object.keys(body).length === 0) {
-    return undefined;
+  contacts[index] = {
+    ...contacts[index],
+    ...body,
   }
 
-  const data = await fs.readFile(contactsPath, 'utf-8');
-  const parsedData = JSON.parse(data);
-
-  const matchedIndex = parsedData.findIndex(
-    contact => contact.id === contactId
-  );
-
-  if (matchedIndex === -1) {
-    return undefined;
-  }
-
-  parsedData[matchedIndex] = { ...parsedData[matchedIndex], ...body };
-
-  fs.writeFile(contactsPath, JSON.stringify(parsedData, undefined, 2));
-
-  return parsedData[matchedIndex];
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
 };
 
 module.exports = {
