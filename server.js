8eb2202278c8dc547844fc89a31aab55c9437290
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-10-03 12:31:19 +0300

    fixed server

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ const { DB_HOST } = process.env;
 
 const PORT = process.env.PORT || 5001;
 
-mongoose.connect(process.env.DB_HOST)
+mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(PORT, ()=>
-    ('Connect on port : ${PORT}'));
-    console.log('Database connect success');
+    app.listen(PORT, () => {
+      console.log (`Connect on port : ${PORT}`);
+      console.log (`Database connect success`)
+    })
   })
   .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
+    console.log(error.message)
+    process.exit(1)
   });
