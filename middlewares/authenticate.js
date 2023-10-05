3e1591db6772ec009016ae7003f599f89c0fa99f
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-10-05 10:13:53 +0300

    up

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const { HttpError } = require("../helpers");
 const { User } = require("../models/user");
 const { SECRET_KEY } = process.env;
+require("dotenv").config();
 
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
